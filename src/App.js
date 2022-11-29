f4a8bd81e49ffd988a83cec3f0e72f22d5b61d74
kalyan cherukuri <112185403+kalyan-cherukuri2812@users.noreply.github.com>
2022-11-29 17:56:49 +0530

    modified:   package-lock.json

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import { faker } from '@faker-js/faker';
 import './App.css';
 
+
+
+
 function App() {
+
+let a=[]
+
+  for (let i = 0;i < 10; i++) {
+    const ID= faker.datatype.uuid();
+    const userName=faker.name.fullName();
+  const address = faker.address.country(); 
+  const age=faker.datatype.number({min: 18, max: 100});
+  const phoneNumber=faker.phone.number();
+  const occupation=faker.name.jobTitle();
+  const vehicalInformation={
+    model:faker.vehicle.vehicle(),
+    carAge:faker.datatype.number({max:10})
+  }
+  a=[...a,{ID,userName,address,age,phoneNumber,occupation,vehicalInformation}]
+  }
+  
+console.log(a)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+   <>
+<h1>hii</h1>
+   </>
   );
 }
 
 export default App;
+
